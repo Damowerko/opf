0f834e3eb0d5b0d5b2336677368ded141e90e442
Damian Owerko <damowerko@gmail.com>
2021-11-05 10:27:15 -0400

    jupyter notebooks

--- paper/utils.py
+++ paper/utils.py
@@ -9,6 +9,10 @@ import pandas as pd
 import tempfile
 import glob
 import os
+from IPython.display import HTML
+import io
+import base64
+import matplotlib.pyplot as plt
 
 
 class CacheOutputs(pl.callbacks.Callback):
@@ -54,3 +58,47 @@ def test(barrier, dm):
     trainer = pl.Trainer(precision=64, callbacks=[cache], logger=False, gpus=0)
     trainer.test(barrier, datamodule=dm, verbose=False)
     return pd.DataFrame(cache.outputs).applymap(torch.Tensor.item)
+
+
+class FlowLayout(object):
+    """A class / object to display plots in a horizontal / flow layout below a cell"""
+
+    def __init__(self):
+        # string buffer for the HTML: initially some CSS; images to be appended
+        self.sHtml = """
+        <style>
+        .floating-box {
+        display: inline-block;
+        margin: 10px;
+        border: 3px solid #888888;  
+        }
+        </style>
+        """
+
+    def add_plot(self, oAxes):
+        """Saves a PNG representation of a Matplotlib Axes object"""
+        Bio = io.BytesIO()  # bytes buffer for the plot
+        fig = oAxes.get_figure()
+        fig.canvas.print_png(Bio)  # make a png of the plot in the buffer
+
+        # encode the bytes as string using base 64
+        sB64Img = base64.b64encode(Bio.getvalue()).decode()
+        self.sHtml += (
+            '<div class="floating-box">'
+            + '<img src="data:image/png;base64,{}\n">'.format(sB64Img)
+            + "</div>"
+        )
+
+    def all_open(self):
+        for i in plt.get_fignums():
+            fig = plt.figure(i)
+            self.add_plot(fig)
+            plt.close(i)
+        return self
+            
+    def _repr_html_(self):
+        return self.sHtml
+
+    def PassHtmlToCell(self):
+        """Final step - display the accumulated HTML"""
+        display(HTML(self.sHtml))
