b702956279c4cd55b578eacf6b925d9e732acc65
Damian Owerko <Damowerko@users.noreply.github.com>
2023-09-05 13:33:42 -0400

    allow zero mlp per gnn

--- scripts/main.py
+++ scripts/main.py
@@ -1,16 +1,14 @@
-#!/usr/bin/python
-
 import argparse
 import os
 import typing
 from functools import partial
+from pathlib import Path
 
 import optuna
 import torch
 from pytorch_lightning import Trainer
-from pytorch_lightning.callbacks import Callback, EarlyStopping, ModelCheckpoint
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
 from pytorch_lightning.loggers.wandb import WandbLogger
-from pytorch_lightning.tuner.tuning import Tuner
 from torchcps.gnn import ParametricGNN
 
 from opf.dataset import CaseDataModule
@@ -23,17 +21,15 @@ def main():
 
     # program arguments
     parser.add_argument("operation", choices=["train", "study"])
-    parser.add_argument("--root_dir", type=str, default="./")
-    parser.add_argument("--log_dir", type=str, default=None)
-    parser.add_argument("--data_dir", type=str, default=None)
-    parser.add_argument("--log", type=int, default=1)
-    parser.add_argument("--wandb", type=int, default=1)
+    parser.add_argument("--log_dir", type=str, default="./logs")
+    parser.add_argument("--data_dir", type=str, default="./data")
+    parser.add_argument("--no_log", action="store_false", dest="log")
     parser.add_argument(
         "--hotstart", type=str, default=None, help="ID of run to hotstart with."
     )
 
     # data arguments
-    parser.add_argument("--case_name", type=str, default="case1354_pegase__api")
+    parser.add_argument("--case_name", type=str, default="case30_ieee")
     parser.add_argument("--batch_size", type=int, default=32)
     parser.add_argument("--fast_dev_run", action="store_true", default=False)
     parser.add_argument("--num_workers", type=int, default=0)
@@ -51,6 +47,9 @@ def main():
 
     params = parser.parse_args()
     params_dict = vars(params)
+
+    # parse the data dir
+
     torch.set_float32_matmul_precision("medium")
     if params.operation == "study":
         study(params_dict)
@@ -60,37 +59,32 @@ def main():
         raise ValueError(f"Unknown operation: {params.operation}")
 
 
-def make_trainer(params):
-    callbacks = []
-    loggers = []
-    run_id = os.environ.get("RUN_ID", None)
+def make_trainer(params, callbacks=[], wandb_kwargs={}):
+    logger = None
     if params["log"]:
-        if params["wandb"]:
-            wandb_logger = WandbLogger(
-                project="opf",
-                id=run_id,
-                save_dir=params["log_dir"],
-                config=params,
-                log_model=True,
-            )
-            wandb_logger.log_hyperparams(params)
-            loggers.append(wandb_logger)
-            run_id = wandb_logger.experiment.id
+        logger = WandbLogger(
+            project="opf",
+            save_dir=params["log_dir"],
+            config=params,
+            log_model=True,
+            **wandb_kwargs,
+        )
+        logger.log_hyperparams(params)
 
         # logger specific callbacks
         callbacks += [
             ModelCheckpoint(
                 monitor="val/loss",
-                dirpath=os.path.join(params["log_dir"], f"tensorboard/{run_id}"),
-                filename=f"{run_id}"
-                + "-epoch={epoch}-loss={val/loss:0.2f}-error={val/inequality/error_max:0.2f}",
+                dirpath=Path(params["log_dir"]) / "checkpoints",
+                filename="best",
                 auto_insert_metric_name=False,
+                mode="min",
+                save_top_k=1,
             ),
         ]
-
     callbacks += [EarlyStopping(monitor="val/loss", patience=params["patience"])]
     trainer = Trainer(
-        logger=loggers,
+        logger=logger,
         callbacks=callbacks,
         accelerator="gpu" if params["gpu"] else "cpu",
         devices=1,
@@ -102,17 +96,6 @@ def make_trainer(params):
 
 
 def train(trainer: Trainer, params):
-    params["data_dir"] = (
-        os.path.join(params["root_dir"], "data")
-        if params["data_dir"] is None
-        else params["data_dir"]
-    )
-    params["log_dir"] = (
-        os.path.join(params["root_dir"], "logs")
-        if params["log_dir"] is None
-        else params["log_dir"]
-    )
-
     dm = CaseDataModule(pin_memory=params["gpu"], **params)
     model = create_model(params)
 
@@ -164,46 +147,41 @@ def objective(trial: optuna.trial.Trial, default_params: dict):
         n_layers=trial.suggest_int("n_layers", 1, 128),
         dropout=trial.suggest_float("dropout", 0, 1),
         heads=8,
-        n_channels=64,
-        mlp_hidden_channels=256,
-        mlp_read_layers=2,
+        n_channels=32,
         activation="leaky_relu",
+        # loss function parameters, should be kept constant within study
         cost_weight=1e-6,
-        equality_weight=1e6,
-        mlp_per_gnn_layers=trial.suggest_int("mlp_per_gnn_layers", 1, 4),
+        equality_weight=1e3,
+        # MLP parameteers
+        mlp_hidden_channels=256,
+        mlp_read_layers=trial.suggest_int("mlp_read_layers", 1, 4),
+        mlp_per_gnn_layers=trial.suggest_int("mlp_per_gnn_layers", 0, 4),
+        enforce_constraints=trial.suggest_categorical(
+            "enforce_constraints", [True, False]
+        ),
     )
     params = {**default_params, **params}
+    trainer = make_trainer(
+        params,
+        callbacks=[
+            optuna.integration.PyTorchLightningPruningCallback(
+                trial, monitor="val/loss"
+            )
+        ],
+        wandb_kwargs=dict(group=trial.study.study_name),
+    )
 
-    # configure trainer
-    logger = WandbLogger(
-        project="opf-1",
-        log_model=True,
-        group=trial.study.study_name,
-    )
-    callbacks: typing.List[Callback] = [
-        ModelCheckpoint(
-            monitor="val/loss",
-            dirpath="./checkpoints",
-            filename="best",
-            auto_insert_metric_name=False,
-            mode="min",
-            save_top_k=1,
-        ),
-        EarlyStopping(monitor="val/loss", patience=10),
-        optuna.integration.PyTorchLightningPruningCallback(trial, monitor="val/loss"),
-    ]
-    trainer = Trainer(
-        logger=logger,
-        callbacks=callbacks,
-        precision=32,
-        max_epochs=params["max_epochs"],
-        accelerator="gpu",
-        devices=1,
-    )
-    train(trainer, params)
+    dm = CaseDataModule(pin_memory=params["gpu"], **params)
+    model = create_model(params)
+    trainer.fit(model, dm)
 
     # finish up
-    trial.set_user_attr("wandb_id", logger.experiment.id)
+    if isinstance(trainer.logger, WandbLogger):
+        trial.set_user_attr("wandb_id", trainer.logger.experiment.id)
+
+    for logger in trainer.loggers:
+        logger.finalize("finished")
+    print(trainer.callback_metrics)
     return trainer.callback_metrics["val/loss"].item()
 
 
