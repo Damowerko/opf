f03729b3c1665d55ad4420d6beaa6df6110f1fdb
Damian Owerko <Damowerko@users.noreply.github.com>
2023-09-28 12:47:06 -0400

    more tests

--- test/test_hetero.py
+++ test/test_hetero.py
@@ -7,17 +7,45 @@ from torch_geometric.transforms import ToSparseTensor
 from opf.hetero import HeteroGCN
 
 
-def test_hetero():
-    dataset = FakeHeteroDataset(avg_num_nodes=100)
-    data = dataset[0]
+@pytest.fixture
+def data():
+    data = FakeHeteroDataset()[0]
     assert isinstance(data, HeteroData)
-    transform = ToSparseTensor()
-    data = transform(data)
+    data = ToSparseTensor()(data)
     assert isinstance(data, HeteroData)
+    return data
 
+
+def check_shape(y_dict, out_channels):
+    for node_type in y_dict:
+        assert y_dict[node_type].shape[-1] == out_channels
+
+
+def test_hetero(data: HeteroData):
     model = HeteroGCN(
         data.metadata(), in_channels=-1, out_channels=10, n_taps=4, mlp_per_gnn_layers=2
     )
     y_dict = model(data.x_dict, data.adj_t_dict)
-    for node_type in y_dict:
-        assert y_dict[node_type].shape[-1] == 10
+    check_shape(y_dict, 10)
+
+
+def test_hetero_no_mlp(data: HeteroData):
+    model = HeteroGCN(
+        data.metadata(), in_channels=-1, out_channels=10, n_taps=4, mlp_per_gnn_layers=0
+    )
+    y_dict = model(data.x_dict, data.adj_t_dict)
+    check_shape(y_dict, 10)
+
+
+def test_hetero_single_layer(data: HeteroData):
+    model = HeteroGCN(
+        data.metadata(), in_channels=-1, out_channels=10, n_taps=4, n_layers=1
+    )
+    y_dict = model(data.x_dict, data.adj_t_dict)
+    check_shape(y_dict, 10)
+
+
+def test_hetero_single_tap(data: HeteroData):
+    model = HeteroGCN(data.metadata(), in_channels=-1, out_channels=10, n_taps=1)
+    y_dict = model(data.x_dict, data.adj_t_dict)
+    check_shape(y_dict, 10)
