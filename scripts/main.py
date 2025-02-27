edad62391524b9bc14e1f99cac08d766bba46ebf
Damian Owerko <damowerko@gmail.com>
2025-02-27 16:56:15 -0500

    parameters for study

--- scripts/main.py
+++ scripts/main.py
@@ -301,16 +301,18 @@ def objective(trial: optuna.trial.Trial, default_params: dict):
     n_channels = 64
     params = dict(
         lr=trial.suggest_float("lr", 1e-6, 1e-2, log=True),
-        lr_dual_shared=trial.suggest_float("lr_dual_shared", 1e-2, 100.0, log=True),
-        lr_dual_pointwise=trial.suggest_float(
-            "lr_dual_pointwise", 1e-2, 100.0, log=True
-        ),
-        wd=trial.suggest_float("wd", 1e-8, 1.0, log=True),
-        wd_dual_shared=trial.suggest_float("wd_dual_shared", 1e-3, 10.0, log=True),
+        lr_dual_shared=trial.suggest_float("lr_dual_shared", 1e-3, 1e3, log=True),
+        lr_dual_pointwise=trial.suggest_float("lr_dual_pointwise", 1e-3, 1e3, log=True),
+        wd=trial.suggest_float("wd", 1e-8, 10.0, log=True),
+        wd_dual_shared=trial.suggest_float("wd_dual_shared", 1e-8, 10.0, log=True),
         wd_dual_pointwise=trial.suggest_float(
-            "wd_dual_pointwise", 1e-3, 10.0, log=True
+            "wd_dual_pointwise", 1e-8, 10.0, log=True
         ),
-        dropout=trial.suggest_float("dropout", 0.0, 0.5),
+        grad_clip_value_pointwise=trial.suggest_float(
+            "grad_clip_value_pointwise", 0, 100
+        ),
+        grad_clip_value_shared=100,
+        dropout=0.0,
         multiplier_type="hybrid",
         cost_weight=trial.suggest_float("cost_weight", 1e-2, 1e2, log=True),
         supervised_weight=0.0,
